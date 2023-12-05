fcb1bb7ba08c013b94eae9ece6c154e20368f170
Benjamin Rae <benjaminrae93@gmail.com>
2023-12-05 17:45:27 +0100

    feat: test index template

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,20 +1,42 @@
 package main
 
 import (
+	"html/template"
+	"io"
 	"net/http"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 )
 
+// https://echo.labstack.com/docs/templates#rendering
+type Template struct {
+	templates *template.Template
+}
+
+func (t *Template) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return t.templates.ExecuteTemplate(w, name, data)
+}
+
 func main() {
 	e := echo.New()
 
+	t := &Template{
+		templates: template.Must(template.ParseGlob("public/views/*.html")),
+	}
+	e.Renderer = t
+
 	e.Use(middleware.Logger())
+
+	e.GET("/", Index)
 
 	e.GET("/ping", func(c echo.Context) error {
 		return c.NoContent(http.StatusOK)
 	})
 
 	e.Logger.Fatal(e.Start(":8080"))
+}
+
+func Index(c echo.Context) error {
+	return c.Render(http.StatusOK, "index.html", nil)
 }
